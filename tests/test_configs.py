285fa47b86b9c1bfeb94cb7b3f743a69d768ed34
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-16 17:42:06 +0530

    Update test_configs.py

--- tests/test_configs.py
+++ tests/test_configs.py
@@ -1,1 +1,6 @@
-sdss
+import unittest
+from configs.config import Config
+class TestConfigs(unittest.TestCase):
+  def test_config(self):
+    config = Config('configs/config.json')
+    self.assertIsNotNone(config.get_api_key())
