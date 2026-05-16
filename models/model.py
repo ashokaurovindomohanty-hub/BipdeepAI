db18195de2e60e6b49ee465380ff6a44f95ac7da
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-16 17:11:46 +0530

    Create model.py

---
+++ models/model.py
@@ -0,0 +1,8 @@
+from sklearn.ensemble import RandomForestClassifier
+class Model:
+  def __init__(self):
+    self.model = RandomForestClassifier()
+  def train(self, X, y):
+    self.model.fit(X, y)
+  def predict(self, X):
+    return self.model.predict(X)
