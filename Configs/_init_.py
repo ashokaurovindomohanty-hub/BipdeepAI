2c8738cda873f180c4836ad1388aa805790ecdb6
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-16 16:56:40 +0530

    Update _init_.py

--- Configs/_init_.py
+++ Configs/_init_.py
@@ -1,1 +1,9 @@
-dhjk
+import json
+class Config:
+  def __init__(self, file_path):
+    self.file_path = file_path
+    self.config = json.load(open(file_path))
+  def get_api_key(self):
+    return self.config['api_key']
+  def get_database_url(self):
+    return self.config['database_url']
