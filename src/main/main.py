86716d2e2f769f3687dd400cb088dab35f2947f1
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-16 23:28:53 +0530

    Update main.py

--- src/main/main.py
+++ src/main/main.py
@@ -2,9 +2,22 @@ from configs.config import Config
 from data.utils import load_data
 from models.model import Model
 def main():
+  # Load the configuration file
   config = Config('configs/config.json')
   data = load_data('data/dataset.csv')
   model = Model()
   model.train(data[:, :-1], data[:, -1])
 if __name__ == '__main__':
   main()
+def main():
+  """
+  Main function.
+  """
+```
+   - In `models/model.py`:
+     ```python
+class Model:
+  def __init__(self):
+    """
+    Initialize the Model class.
+    """
