8b4ac8a8246f0b709eda67055f92eba9a5e270ac
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-16 17:39:47 +0530

    Update test_agents.py

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,1 +1,6 @@
-sdfs
+import unittest
+from agents.abstract_agent import AbstractAgent
+class TestAgents(unittest.TestCase):
+  def test_abstract_agent(self):
+    with self.assertRaises(TypeError):
+      AbstractAgent()
