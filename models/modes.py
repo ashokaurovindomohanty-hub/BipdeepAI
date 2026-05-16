cf3559f0c2ed966126e367c647e41cdc28bab712
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-16 17:01:15 +0530

    Update modes.py

--- models/modes.py
+++ models/modes.py
@@ -1,1 +1,8 @@
-dhg
+from sklearn.ensemble import RandomForestClassifier
+class Model:
+  def __init__(self):
+    self.model = RandomForestClassifier()
+  def train(self, X, y):
+    self.model.fit(X, y)
+  def predict(self, X):
+    return self.model.predict(X)
