7216ea40f12b363ed0dc0818a5b2a64e9ebef9f8
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-16 17:08:56 +0530

    Delete models/modes.py

--- models/modes.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-class Model:
-  def __init__(self):
-    self.model = RandomForestClassifier()
-  def train(self, X, y):
-    self.model.fit(X, y)
-  def predict(self, X):
-    return self.model.predict(X)
