be4bd4daaf54eab436cb67feb14dd1e6769fa845
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-16 17:52:30 +0530

    Update _init_.py

--- tests/_init_.py
+++ tests/_init_.py
@@ -1,1 +1,10 @@
-dfhg
+from .test_agents import TestAgents
+from .test_configs import TestConfigs
+from .test_data import TestDatas  # we will add this file 
+from .test_models import TestModels  # we will add this file 
+__all__ = [
+    'TestAgents',
+    'TestConfigs',
+    'TestDatas',
+    'TestModels'
+]
