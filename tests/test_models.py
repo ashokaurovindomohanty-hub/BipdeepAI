ffa7d097020cdc804e0313af39bef6e7b3dd798a
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-17 13:48:18 +0530

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,13 @@
+python
 import unittest
 from models.model import Model
-class TestModels(unittest.TestCase):
-  def test_model(self):
-    model = Model()
-    self.assertIsNotNone(model)
+class TestModel(unittest.TestCase):
+  def setUp(self):
+    self.model = Model()
+  def test_init(self):
+    self.assertIsNotNone(self.model.data)
+    self.assertIsNotNone(self.model.X)
+    self.assertIsNotNone(self.model.y)
+  def test_train(self):
+    self.model.train(self.model.X, self.model.y)
+    self.assertIsNotNone(self.model.model)
