e4c30b8757fe59b6bda7034a6702e220b85184d9
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-17 14:10:12 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -6,12 +6,10 @@ class Model:
     self.model.fit(X, y)
   def predict(self, X):
     return self.model.predict(X)
-python
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, classification_report
-python
 class Model:
   def __init__(self):
     # Load dataset
@@ -19,8 +17,6 @@ class Model:
     # Preprocess data (assuming last column is target variable)
     self.X = self.data.iloc[:, :-1]
     self.y = self.data.iloc[:, -1]
-
-python
 from sklearn.model_selection import train_test_split
 class Model:
   # ...
@@ -28,7 +24,6 @@ class Model:
     # ...
     # Split data into training and testing sets
     self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
-python
 class Model:
   # ...
   def __init__(self):
@@ -36,7 +31,6 @@ class Model:
     # Define hyperparameters for tuning
     self.param_grid = {'n_estimators': [10, 50, 100, 200],
                        'max_depth': [None, 5, 10, 15]}
-python
 from sklearn.model_selection import GridSearchCV
 class Model:
   # ...
@@ -47,12 +41,10 @@ class Model:
     self.grid_search.fit(self.X_train, self.y_train)
     # Get best estimator
     self.model = self.grid_search.best_estimator_
-python
 class Model:
   # ...
   def train(self, X_train, y_train):
     self.model.fit(X_train, y_train)
-python
 from data.utils import load_data
 class Model:
   def __init__(self, file_path='data/dataset.csv'):
