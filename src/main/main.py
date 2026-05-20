8a698e27e9b7aaaf836049ddfc92cfe9b1a0c2a4
Ashok Aurovindo Mohanty <ashokaurovindomohanty@gmail.com>
2026-05-20 21:22:54 +0530

    main.py updated for deployment
    main.py conflict resolved and updated for deployment

--- src/main/main.py
+++ src/main/main.py
@@ -2,10 +2,11 @@ from utils.api_utils import call_api
 from utils.data import load_data
 from models.model import Model
 def main():
-  model = Model()
-  # Load the configuration file
-  config = Config('configs/config.json')
-  # Initialize and train the model
-  model.train(model.X, model.y)
+    model = Model()
+    # Load the configuration file
+    config = Config('configs/config.json')
+    # Initialize and train the model
+    model.train(model.X, model.y)
+    print(call_api("https://api.github.com/user/repos"))
 if __name__ == '__main__':
-  main()
+    main()
